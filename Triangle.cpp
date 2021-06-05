41d391e5d2760bd926dca19d73d34054aa064dd4
Tatianka-hk <78912196+Tatianka-hk@users.noreply.github.com>
2021-06-05 23:29:31 +0300

    Create Triangle.cpp

---
+++ Triangle.cpp
@@ -0,0 +1,27 @@
+#include "Triangle.h"
+inline void Triangle::maxCoordinate(float* _X, float* _Y, float* _Z) {
+	if (p1.x < p2.x) {
+		if (p2.x < p3.x) { *_X = p3.x;}
+		else { *_X = p2.x; }
+	}
+	else { 
+		if (p1.x<p3.x){ *_X = p3.x; }
+		else {* _X = p1.x; }
+	}
+	if (p1.y < p2.y) {
+		if (p2.y < p3.y) {*_Y = p3.y; }
+		else {* _Y = p2.y; }
+	}
+	else {
+		if (p1.y < p3.y) {* _Y = p3.y; }
+		else { *_Y = p1.y; }
+	}
+	if (p1.z < p2.z) {
+		if (p2.z < p3.z) { *_Z = p3.z; }
+		else { *_Z = p2.z; }
+	}
+	else {
+		if (p1.z < p3.z) { *_Z = p3.z; }
+		else { *_Z = p1.z; }
+	}
+}
