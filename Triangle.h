182c415607a32aec004727add94cad41ba39c4d4
Tatianka-hk <78912196+Tatianka-hk@users.noreply.github.com>
2021-06-06 21:26:17 +0300

    Update Triangle.h

--- Triangle.h
+++ Triangle.h
@@ -7,8 +7,8 @@ class Triangle {
 public:
 	Point p1, p2, p3;
 	colorRGB color;
-	Triangle();
+	Triangle(){};
 	Triangle(Point p1, Point p2, Point p3) { this->p1 = p1; this->p2 = p2; this->p3 = p3; }
 	void maxCoordinate(float* _X, float* _Y, float* _Z);
-	~Triangle();
+	~Triangle(){};
 };
