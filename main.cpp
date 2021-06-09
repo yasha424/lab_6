f1407f7c6557c8f0c2184a54a177e72f9b24754c
yasha424 <jakob20022404@gmail.com>
2021-06-10 00:55:29 +0300

    working, but without tree

--- main.cpp
+++ main.cpp
@@ -14,9 +14,9 @@ using namespace std;
 
 int main(){
     vector <Triangle> triangles = create_triangles("samples/cow.obj");
-    int size = 20;
-    double gap = 0.1;
-    Camera camera(Point(200, 200, 10), Point(190, 190, 10), size, gap);
+    int size = 40;
+    double gap = 0.05;
+    Camera camera(Point(-1, -1, 0), Point(0, 0, 0), size, gap);
 
     vector <vector<Pixel>> pixels;
     //
@@ -32,46 +32,49 @@ int main(){
     //     pixels.push_back(pixels_1);
     // }
 
-    // bool intersect = false;
+    bool intersect = false;
+
+    for (size_t i = 0; i < size * 2 + 1; i++) {
+        vector <Pixel> pixels_1;
+        for (size_t j = 0; j < size * 2 + 1; j++) {
+            for (size_t k = 0; k < triangles.size(); k++) {
+                Plane plane(triangles[k]);
+                Point point = plane.getPointIntersection(Point(-1, -1, 0), Point(0 - (gap * size) + (gap * j), 0, 0  - (gap * size) + (gap * i)));
+                if (point.intersection(triangles[k].p1, triangles[k].p2, triangles[k].p3)) {
+                    intersect = true;
+                    break;
+                }
+            }
+            if (intersect) {
+                Pixel pixel(255, 255, 255);
+                pixels_1.push_back(pixel);
+                cout << "x";
+            } else {
+                Pixel pixel(0, 0, 0);
+                pixels_1.push_back(pixel);
+                cout << ".";
+            }
+            intersect = false;
+        }
+        cout << endl;
+        pixels.push_back(pixels_1);
+    }
+
+    Image image(81, 81, pixels);
+    image.write("test.bmp");
+
+    // Point p1(0, 0, 3);
+    // Point p2(5, 0, 0);
+    // Point p3(0, 5, 0);
     //
-    // for (size_t i = 0; i < size * 2 + 1; i++) {
-    //         vector <Pixel> pixels_1;
-    //     for (size_t j = 0; j < size * 2 + 1; j++) {
+    // Point cum(0, 0, 3);
+    // Point obj(-1, -1, 2);
+    // Plane plane(p1, p2, p3);
     //
-    //         for (size_t k = 0; k < triangles.size(); k++) {
-    //             Plane plane(triangles[k]);
-    //             Point point = plane.getPointIntersection(Point(0, 0, 0), Point(2 - (gap * size) + (gap * j), 2, 0  - (gap * size) + (gap * i)));
-    //             if (point.intersection(triangles[k].p1, triangles[k].p2, triangles[k].p3)) {
-    //                 intersect = true;
-    //                 break;
-    //             }
-    //         }
-    //         if (intersect) {
-    //             Pixel pixel(255, 255, 255);
-    //             pixels_1.push_back(pixel);
-    //         } else {
-    //             Pixel pixel(0, 0, 0);
-    //             pixels_1.push_back(pixel);
-    //         }
-    //     }
-    //     pixels.push_back(pixels_1);
-    // }
+    // Point inter = plane.getPointIntersection(cum, obj);
     //
-    // Image image(41, 41, pixels);
-    // image.write("test.bmp");
-
-    Point p1(1, 0, 0);
-    Point p2(5, 0, 0);
-    Point p3(0, 5, 0);
-
-    Point cum(0, 0, 3);
-    Point obj(0, 0, 2);
-    Plane plane(p1, p2, p3);
-
-    Point inter = plane.getPointIntersection(cum, obj);
-
-    cout << inter.x << ", " << inter.y << ", " << inter.z << endl;
-
-    cout << inter.intersection(p1, p2, p3) << endl;
-
+    // cout << inter.x << ", " << inter.y << ", " << inter.z << endl;
+    //
+    // cout << inter.intersection(p1, p2, p3) << endl;
+    //
 }
