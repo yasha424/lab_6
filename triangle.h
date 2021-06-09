d6a8a783a32729bb6c801e007003f1c2ac364eb8
Tatianka-hk <78912196+Tatianka-hk@users.noreply.github.com>
2021-06-09 20:22:10 +0300

    Update triangle.h

--- triangle.h
+++ triangle.h
@@ -1,11 +1,14 @@
 #pragma once
 #include <iostream>
 #include "point.h"
+struct colorRGB {
+	float red, green, blue;
+};
 
 class Triangle{
 public:
     Point p1, p2, p3;
-
+    colorRGB color;
     Triangle(){}
 
     Triangle(Point, Point, Point);
